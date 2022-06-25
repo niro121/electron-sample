90604689eb519fcead174ba3c0d929222fa1794e
Nirosha <nirosha@archmage.lk>
2022-06-25 22:12:43 +0530

    niro

---
+++ src/Componanets/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function about() {
+  return (
+    <div><h1>Welcome to About</h1>    
+    <br></br>
+    <a href='#/'> HOME </a>
+    </div>
+  )
+}
+
+export default about
---
+++ src/Componanets/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div><h1>Welcome to Home</h1>
+    <br></br>
+    <a href='#/about'> ABOUT </a>
+    </div>
+  )
+}
+
+export default home
